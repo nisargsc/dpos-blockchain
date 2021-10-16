9698dcbecfa637d8a83cb93e0f26b6fe4dff5c72
Nisarg Chaudhari <nisarg5601@gmail.com>
2021-10-16 15:12:33 +0530

    Core Server added
    * core-server added
    
    * gitignore modified

---
+++ core-server/app.py
@@ -0,0 +1,29 @@
+from core import Core
+from flask import Flask, jsonify
+
+core = Core()
+
+app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+
+@app.route('/', methods=['GET'])
+def demo():
+    response = {
+        'message' : 'Core is runnig!!',
+    }
+    return jsonify(response), 200
+
+def register_node():
+    pass
+
+def add_candidate():
+    pass
+
+def show_candidates():
+    pass
+
+def vote():
+    pass
+
+if __name__ == '__main__':
+    app.run(port=4000)
---
+++ core-server/core.py
@@ -0,0 +1,21 @@
+
+class Core():
+    def __init__(self):
+        self.nodes = {}
+        self.candidates = {}
+        self.block_creators = []
+
+def register_node(self):
+    pass
+
+def add_candidate(self):
+    pass
+
+def add_vote(self):
+    pass
+
+def update_block_creators(self):
+    pass
+
+if __name__ == '__main__':
+    pass
