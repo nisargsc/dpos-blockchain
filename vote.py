df600b07593294ac1e9c74a0fb6fd90be500de25
soumya pochiraju <soumya.pochiraju@gmail.com>
2021-10-16 18:37:05 +0530

    "vote.py" class added which contains the voting information -digitally signed vote -public key of the node that cast the vote

---
+++ vote.py
@@ -0,0 +1,36 @@
+import datetime
+import json
+
+class Vote():
+
+	def __init__(self, vote:str, public_key:str):
+		self.vote = vote
+		self.public_key = public_key
+
+    def dict(self):
+        """
+        :return: <dict> python dictionary for the vote details
+        """
+        vote_dict = {
+            'vote' : self.vote,
+            'public_key' : self.public_key
+        }
+        return vote_dict
+
+    def json(self):
+        """
+        :return: <json> json responce for the vote details
+        """
+        vote_json = json.dumps(self.dict(), indent=4)
+        return vote_json
+        
+    def __str__(self):
+        """
+        Gets called every time vote object is converted to string
+
+        :return: <str> String of the json responce
+        """
+        return str(self.json())
+
+
+
