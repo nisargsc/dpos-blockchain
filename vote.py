d57f8491eeac1e28268bbc037152e6ed4dfcb32b
Nisarg Chaudhari <nisarg5601@gmail.com>
2021-10-25 23:52:00 +0530

    vote and candidate system added (#6)

--- vote.py
+++ vote.py
@@ -2,17 +2,19 @@ import json
 
 class Vote():
 
-    def __init__(self, vote:str, public_key:str):
+    def __init__(self, vote:str, id:str, sign):
         self.vote = vote
-        self.public_key = public_key
+        self.id = id
+        self.sign = sign
 
     def dict(self):
         """
         :return: <dict> python dictionary for the vote details
         """
         vote_dict = {
+            'id' : self.id,
             'vote' : self.vote,
-            'public_key' : self.public_key
+            'sign' : self.sign
         }
         return vote_dict
 
